7cefd16a01c76e65553a7e1946d3bad7b7a3f5ad
ohmycloud <ohmycloudy@gmail.com>
2024-11-23 18:56:08 +0800

    parse str as int

---
+++ cognition/examples/parse-int-err.rs
@@ -0,0 +1,32 @@
+use std::num::ParseIntError;
+
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
+    Ok(qty * cost_per_item + processing_fee)
+}
+
+fn main() {
+    if let Ok(cost) = total_cost("42") {
+        println!("{}", cost);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn item_quantity_is_a_valid_number() {
+        assert_eq!(total_cost("34"), Ok(171));
+    }
+
+    #[test]
+    fn item_quantity_is_an_invalid_number() {
+        assert_eq!(
+            total_cost("beep boop").unwrap().to_string(),
+            "invalid digit found in string"
+        )
+    }
+}
